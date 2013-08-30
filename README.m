1b5350e04b4c4caf4a7b367b2f1a030366c9a336
maykdq <maykdq@gmail.com>
2013-08-30 17:47:12 -0500

    README.m

---
+++ README.m
@@ -0,0 +1,28 @@
+### TareaLaboratorio 1 - Evidencia Tarea
+
+1. Elaborar los laboratorios que se encuentran en el fascículo 1.
+   Lab_1_1
+   Lab_1_2
+   Lab_1_3
+
+3. Realizar ejercicio de interfaz gráfica.
+   Graf_1
+
+2. Realizar la actividad de la pagina 30 del fascículo 1.
+   Fas_1_1
+3. Comprimir y enviar evidencias a través de este enlace.
+
+
+### TareaAutoevaluación 1 - Evidencia Tarea
+
+1. Realizar la autoevaluación del fascículo 1.
+   Eval_1_1
+   Eval_1_2
+   Eval_1_3
+
+2. Comprimir y enviar evidencia a través de este enlace.
+
+
+### TareaProyecto - Evidencia 1 Tarea
+
+Adjuntar documento con el avance realizado en clase sobre el proyecto planteado en la Agenda del Curso.
